b5324338e6370353b75ac7e472b578bdb1e52449
Giovanni Flores <geflores5@gmail.com>
2017-04-02 11:10:03 +0000

    Add access to lessons requirement

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,24 @@
 class LessonsController < ApplicationController
-  def show
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
+  def show
   end
 
+  private
+
   helper_method :current_lesson
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+    def current_lesson
+      @current_lesson ||= Lesson.find(params[:id])
+    end
+
+  def current_course
+    @current_course ||= current_lesson.section.course
+  end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user && !current_user.enrolled_in?(current_course)
+        redirect_to course_path(current_course), alert: 'You are not enrolled in this course'
+    end
   end
 end
